a10147e3caa7b70f215e8bba2f6aa3457581c027
Boris Bera <bboris@rsoft.ca>
2015-03-11 12:15:00 -0400

    removed restart from hh db usage

--- lib/hidden_hippo/cli/app.rb
+++ lib/hidden_hippo/cli/app.rb
@@ -4,7 +4,7 @@ require 'hidden_hippo/cli/database'
 module HiddenHippo
   module Cli
     class App < Thor
-      desc 'db start|stop|restart|status', 'control the database service'
+      desc 'db start|stop|status', 'control the database service'
       subcommand 'db', Database
     end
   end
