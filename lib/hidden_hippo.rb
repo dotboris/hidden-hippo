97741adfcca947cf55360d9d92301dc31520dfba
Boris Bera <bboris@rsoft.ca>
2015-03-13 14:56:51 -0400

    Now configuring mongoid

--- lib/hidden_hippo.rb
+++ lib/hidden_hippo.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 require 'hidden_hippo/version'
 require 'hidden_hippo/cli/app'
 
@@ -9,4 +10,14 @@ module HiddenHippo
     return false
   end
   module_function :pid_exists?
+
+  def configure_db!(env = :production)
+    Mongoid.load! gem_root + 'config/mongoid.yml', env
+  end
+  module_function :configure_db!
+
+  def gem_root
+    Pathname.new(__FILE__) + '../../'
+  end
+  module_function :gem_root
 end
