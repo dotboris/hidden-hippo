9f1edfbf730183bf66ea9d932a6e158a9cfb148a
Boris Bera <bboris@rsoft.ca>
2015-03-27 13:34:37 -0400

    Added index page showing all names

--- lib/hidden_hippo/gui.rb
+++ lib/hidden_hippo/gui.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
+require 'hidden_hippo'
+require 'hidden_hippo/dossier'
 
 module HiddenHippo
   class Gui < Sinatra::Application
+    set :root, proc { (HiddenHippo.gem_root + 'gui').to_s }
+
     get '/' do
-      'hello world'
+      dossiers = Dossier.all
+
+      erb :index, locals: {dossiers: dossiers}
     end
   end
 end
